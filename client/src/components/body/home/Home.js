b5614685bf61a83a859b4c9d942bc6494427d853
hamzazaheer721 <hamzazaheer721@gmail.com>
2020-11-25 11:10:03 +0500

    MERN Stack Login System

---
+++ client/src/components/body/home/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+    return (
+        <div className="home_page">
+            <h2>Hello everyone!</h2>
+            <p>
+                This site is about user authentication, 
+                so there won't be any other pages here. 
+                If people want to see more about how to 
+                create other websites. You can click on 
+                the link below, visit my Github Profile. 
+                
+            </p>
+            <a href="https://github.com/Hamzazaheer721" target="_blank" 
+            rel="noopener noreferrer">Github</a>
+
+            
+        </div>
+    )
+}
+
+export default Home
